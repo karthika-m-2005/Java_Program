050071fbed3a910a58022fa59992ee93bda3f072
KARTHIKA M <km646383@gmail.com>
2025-01-30 22:00:05 +0530

    Add Three Number in Java

---
+++ Add_Three.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Add_Three
+  {
+    public static void main(String args[])
+    {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+      int d = a+b+c:
+      System.out.println("Sum of Three Number:"+d);
+      sc.close();
+    }
+  }
