218cfb25c63da644214712e08f51fd9f05ff8e0d
KARTHIKA M <km646383@gmail.com>
2025-02-01 20:35:44 +0530

    Find the Largest Number  3 Numbers in java

---
+++ Find_Large_Num_pro.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Find_Large_Num_pro
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+
+    if(a>b && a>c)
+    {
+      System.out.pritnln(a+" is a greatest Number");
+    }
+      else if(b>a && b>c)
+      {
+        System.out.println(b+" is a greatest Number");
+      }
+      else
+      {
+        System.out.println(c+" is a agreatest Number");
+      }
+      sc.close();
+    }
+  }
