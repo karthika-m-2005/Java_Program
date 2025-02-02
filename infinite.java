faac3c8d78bc1ad32a05dd0f74320a3793c4ac01
KARTHIKA M <km646383@gmail.com>
2025-02-02 21:46:30 +0530

    Create infinite program  in java

---
+++ infinite.java
@@ -0,0 +1,15 @@
+public class infinite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		while(true)
+		{
+			System.out.println("Karthika");
+			
+		}
+		
+		
+	}
+
+}
