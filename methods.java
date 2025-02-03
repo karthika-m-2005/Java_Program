cc2894fe05ee8e9848df8b2ef4e37f369fee2250
KARTHIKA M <km646383@gmail.com>
2025-02-03 22:53:26 +0530

    Create methods program in java

---
+++ methods.java
@@ -0,0 +1,19 @@
+
+ public class Main
+ {
+     static  int BoxVolume(int i, int j, int k) {
+		int vol = i*j*k;
+		return vol;
+	}
+    
+    public static void main(String[] args)
+    {
+      System.out.println("Hello");
+     int m = BoxVolume(10,20,30);//arguments parsing
+     System.out.println(m);
+    }
+
+	
+  }
+
+      
