a46bbf6f546860af51c36ff185537ca122b44e8e
KARTHIKA M <km646383@gmail.com>
2025-02-05 18:46:00 +0530

    Create Natural_Number Program injava

---
+++ Natural_Number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Natural_Number
+  {
+   public static void main(String args[])
+    {
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter the Number:");
+      int num = sc.nextInt();
+      for(int i = 1;i<=num;i++)
+        {
+          System.out.print(i+" ");
+        }
+    }
+  }
