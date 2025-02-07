221b125be5d4b5070748147ca47645bc1822876a
KARTHIKA M <km646383@gmail.com>
2025-02-07 18:46:14 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main
+  {
+       static void printline()
+    {
+      System.out.println("==========================");
+    }
+    public static void main(String args[])
+    {
+      printline();
+      System.out.println("Hello Karthika");
+      printline();
+    }
+  }
+    
