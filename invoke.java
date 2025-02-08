7771f1b72c68668d62e78f1acd5aa137f39a4976
KARTHIKA M <km646383@gmail.com>
2025-02-08 21:27:41 +0530

    invoke method in java

---
+++ invoke.java
@@ -0,0 +1,33 @@
+package firstproject;
+import java.util.Scanner;
+
+class Demo {
+	int h;
+	int l;
+	
+	int surfaceArea()
+	{
+		return h*l;
+	}
+}
+class invoke
+{
+
+	public static void main(String[] args)
+     {
+    	 
+	 Scanner sc = new Scanner(System.in);
+	 Demo box = new Demo();
+	 System.out.println("Enter the Height: ");
+	 box.h = sc.nextInt();
+	 System.out.println("Enter the Length: ");
+	 box.l = sc.nextInt();
+	 System.out.println("SurfaceArea : "+box.surfaceArea());
+	 sc.close();
+	 
+     }
+	
+	
+}
+
+
