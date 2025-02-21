34ac5a2e1ab81fdb4d9ba17cb8b4fa8ae63663e9
KARTHIKA M <km646383@gmail.com>
2025-02-21 18:29:09 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Swap
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a Value: ");
+    int a = sc.nextInt();
+    System.out.println("Enter b Value: ");
+    int b = sc.nextInt();
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.println("A Value: "+a);
+    System.out.println("B Value: "+b);
+  }
+}
