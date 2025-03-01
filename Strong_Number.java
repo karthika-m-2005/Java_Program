e7cfca70fa50a08abae18cf57615cc48fbf8e151
karthika-m-2005 <km646383@gmail.com>
2025-03-01 17:25:41 +0530

    check strong number program in jva

---
+++ Strong_Number.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Strong_Number {
+	
+	public static int fact(int n)
+	{
+		if(n==0 || n==1)
+		   return 1;
+		return n*fact(n-1);
+		
+	}
+	
+	
+	public static boolean strongnumber(int n)
+	{
+		int sum = 0;
+		int temp = n;
+		while(n>0)
+		{
+			
+		    int digit=n%10;
+		    sum = sum+fact(digit);
+		    n = n/10;
+			
+		}
+		
+		return (sum == temp)?true:false;
+		
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number: ");
+		int n = sc.nextInt();
+		
+		System.out.println(n+ " is a Strong Number(true/false): "+ strongnumber(n));
+		sc.close();
+		
+
+	}
+
+}
