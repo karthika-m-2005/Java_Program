3c612daac2227d47d0ade5983118a3a74ea1a696
KARTHIKA M <km646383@gmail.com>
2025-03-08 20:05:55 +0530

    Updated

--- Add_Three.java
+++ Add_Three.java
@@ -4,8 +4,11 @@ class Add_Three
     public static void main(String args[])
     {
       Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the a value:");
       int a = sc.nextInt();
+      System.out.println("Enter the b value:");
       int b = sc.nextInt();
+      System.out.println("Enter the c value:");
       int c = sc.nextInt();
       int d = a+b+c;
       System.out.println("Sum of Three Number:"+d);
