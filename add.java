5fb96adf1960efbcca3aee6492cdc956e53517e0
KARTHIKA M <km646383@gmail.com>
2025-03-08 20:06:41 +0530

    Updated

--- add.java
+++ add.java
@@ -4,7 +4,9 @@ class add
     public static void main(String args[])
     {
       Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the a value:");
       int a = sc.nextInt();
+      System.out.println("Enter the b value:");
       int b = sc.nextInt();
       int c = a+b;
       System.out.println("Sum of a and b:"+c);
