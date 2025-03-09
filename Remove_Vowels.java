427c6d5b4a82bcfebc4b7138309d3b826ff8d2f1
KARTHIKA M <km646383@gmail.com>
2025-03-09 21:49:23 +0530

    Update Remove_Vowels.java

--- Remove_Vowels.java
+++ Remove_Vowels.java
@@ -24,6 +24,7 @@ class Remove_Vowels {
 public static void main(String[] args)
   {
   Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the String: ");
   String s = sc.nextLine();
   System.out.print(removeVowels(s));
   sc.close();
