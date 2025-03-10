c1f6509dcbcc6cef481f977b74d8f247c555991c
KARTHIKA M <km646383@gmail.com>
2025-03-10 22:11:49 +0530

    Fibonacci_series.java

--- Fibonacci_series.java
+++ Fibonacci_series.java
@@ -7,6 +7,7 @@ public class Fibonacci_series {
 		int num1 = 0;
 		int num2 = 1;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
 		int n=sc.nextInt();
 		System.out.print(num1+" ");
 		for(int i = 0; i<=n;i++)
