1d6f0c397a7bd742062fad9c66a78466e7407058
KARTHIKA M <km646383@gmail.com>
2025-03-10 22:11:16 +0530

    Find_Large_Num_pro.java

--- Find_Large_Num_pro.java
+++ Find_Large_Num_pro.java
@@ -4,8 +4,11 @@ class Find_Large_Num_pro
     public static void main(String[] args)
     {
       Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a Value: ");
       int a = sc.nextInt();
+      System.out.println("Enter b Value: ");
       int b = sc.nextInt();
+      System.out.println("Enter c Value: ");
       int c = sc.nextInt();
 
     if(a>b && a>c)
