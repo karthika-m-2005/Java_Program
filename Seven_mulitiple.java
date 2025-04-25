7c39d51008540f5e59b44d24ebbec378b526d502
KARTHIKA M <km646383@gmail.com>
2025-04-25 22:12:57 +0530

    Create Seven_mulitiple.java

---
+++ Seven_mulitiple.java
@@ -0,0 +1,32 @@
+package Javacourse;
+
+public class Seven_mulitiple {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		int prod;
+		
+	    while(true)
+	    {
+	    	prod = i*7;
+	    	
+	    	
+    	   
+	        if(prod<999)
+	        {
+	        	 i++;
+	             System.out.print(prod+" ");
+	        }
+	        else
+	        	break;
+	    
+	    	    
+	    	
+	    	
+	    }
+	    
+
+	}
+
+}
