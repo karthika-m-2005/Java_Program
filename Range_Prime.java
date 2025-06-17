f10a89fdc6a5dfdb7662840e170053e33ce8c671
KARTHIKA M <km646383@gmail.com>
2025-06-17 22:41:35 +0530

    Create Range_Prime.java

---
+++ Range_Prime.java
@@ -0,0 +1,9 @@
+import java.uti.Scanner;
+public class Range_Prime
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in)
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      System.out.
